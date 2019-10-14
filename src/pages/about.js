63d488232c6e5d117185285de0f28ecf1e75e12b
ssapienza <sapienza@umd.edu>
2019-10-14 15:57:34 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,31 +6,35 @@ const AboutPage = () => (
   <Layout url="/about/">
     <h1>About</h1>
     <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    mollit anim id est laborum.
-    </p>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    mollit anim id est laborum.
-    </p>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    mollit anim id est laborum.
+    Unlocking the Airwaves: Revitalizing an Early Public and Educational
+Radio Collection is a comprehensive online collection of early educational
+public radio content from the National Association of Educational
+Broadcasters (NAEB). The forerunner of CPB and its arms, NPR and
+PBS, the NAEB developed and distributed educational radio programs
+and accompanying print materials to schools and communities across
+the United States. What’s more, the NAEB lobbied extensively to unlock
+the airwaves—to access precious frequency space—in order to bring the
+voices of poet Robert Frost, architect Frank Lloyd Wright, anthropologist
+Margaret Mead, and conservationist “Ranger Mac,” among many other
+individuals, into American homes and classrooms.</p>
+<p>The NAEB’s history is the dramatic story of idealists who believed in the
+utopian possibilities of technology for education and social uplift and
+who faced considerable challenges in pursuit of those goals, including
+economic depression, world war, and the scarcity of the electromagnetic
+spectrum. It’s a story that has much to tell us about 20th century
+American culture, as well as the 21st century’s environment of online
+educational technology and podcasting that we live in today.</p>
+<p>Despite its historic importance and contemporary relevance, most of the
+NAEB members’ programs were never heard again after their initial brief
+moments on the air. The archives for the radio programs and their related
+paper documentation have been split for over 25 years between two
+institutions: the University of Maryland and the Wisconsin Historical
+Society. Unlocking the Airwaves reunites the split collections, finally
+realizing the potential of the collections of the NAEB for exploration and
+study by educators, scholars, journalists, documentarians, genealogists,
+and the broader public.</p>
+<p>Click on About to read more about the project and the NAEB. To access
+radio programs and related archival materials, click Explore the Archive.
     </p>
   </Layout>
 )
