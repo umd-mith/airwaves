e815527c15965ce6bc80743bc4e4200f48b01fae
Ed Summers <ehs@pobox.com>
2019-10-29 17:50:47 -0400

    facet counting; but not actionable yet

--- src/components/search-facets.js
+++ src/components/search-facets.js
@@ -6,36 +6,36 @@ class SearchFacets extends Component {
 
   render() {
 
+    const facets = getFacets(this.props.results)
+
     return (
       <div className="facets">
 
 		    <div className="facet-panel item-total">
-			    Refine Results <span className="item-count">1111</span>
+			    Refine Results <span className="item-count">{this.props.results.length}</span>
 		    </div>
 
+        {facets.map(facet => (
         <div className="facet-panel facet-type">
-          <label className="facet-label facet-label-type">Filter By Type</label>
+          <label className="facet-label facet-label-type">Filter By {facet.name}</label>
           <dl className="facet-list">
-            <dd className="item-count">999</dd>
-            <dt>
-              <input type="radio" name="item-type" value="all" defaultChecked={true} 
-                  onClick={this.setCategory} />
-              <label title="All">All</label>
-            </dt>
-            <dd className="item-count">999</dd>
-            <dt>
-              <input type="radio" name="item-type" value="episodes" 
-                  onClick={this.setCategory} />
-              <label title="Media">Media</label>
-            </dt>
-            <dd className="item-count">99</dd>
-            <dt>
-              <input type="radio" name="item-type" value="documents"
-                  onClick={this.setCategory} />
-              <label title="Documents">Documents</label>
-            </dt>
+            {facet.counts.map((f, i) => (
+              <React.Fragment key={`facet-${facet.name}-${i}`}>
+                <dd className="item-count">{f[1]}</dd>
+                <dt>
+                  <input
+                    type="checkbox"
+                    name="item-type"
+                    value="all"
+                    defaultChecked={true} 
+                    onClick={this.setCategory} />
+                  <label title="{f[0]}">{f[0]}</label>
+                </dt>
+              </React.Fragment>
+            ))}
           </dl>
         </div>
+        ))}
 
         <div className="facet-panel facet-decade">
           <label className="facet-label facet-label-decade">Filter by Decade</label>
@@ -53,64 +53,72 @@ class SearchFacets extends Component {
           </dl>
         </div>
 
-        <div className="facet-panel facet-expandable facet-subject">
-          <label className="facet-label facet-label-subject">Filter by Subject</label>
-          <dl className="facet-list">
-            <dd className="item-count">777</dd>
-            <dt>
-              <input type="checkbox" name="item-subject" value="all" defaultChecked={true} onClick={this.setCategory} />
-              <label title="All">All</label>
-            </dt>
-            <dd className="item-count">77</dd>
-            <dt>
-              <input type="checkbox" name="item-subject" value="s1" onClick={this.setCategory} />
-              <label title="s1">Subject One</label>
-            </dt>
-          </dl>
-        </div>
-
-        <div className="facet-panel facet-expandable facet-creator">
-          <label className="facet-label facet-label-creator">Filter by Creator</label>
-          <dl className="facet-list">
-            <dd className="item-count">888</dd>
-            <dt>
-              <input type="checkbox" name="item-creator" value="all" defaultChecked={true} onClick={this.setCategory} />
-              <label title="All">All</label>
-            </dt>
-            <dd className="item-count">88</dd>
-            <dt>
-              <input type="checkbox" name="item-creator" value="c1" onClick={this.setCategory} />
-              <label title="c1">Creator Name</label>
-            </dt>
-          </dl>
-        </div>
-
-        <div className="facet-panel facet-expandable facet-genre">
-          <label className="facet-label facet-label-genre">Filter by Genre</label>
-          <dl className="facet-list">
-            <dd className="item-count">555</dd>
-            <dt>
-              <input type="checkbox" name="item-genre" value="all" defaultChecked={true} onClick={this.setCategory} />
-              <label title="All">All</label>
-            </dt>
-            <dd className="item-count">55</dd>
-            <dt>
-              <input type="checkbox" name="item-genre" value="g1" onClick={this.setCategory} />
-              <label title="g1">Genre One</label>
-            </dt>
-          </dl>
-        </div>
-
-		    <div>
-          <label className="facet-label">
-            <Link to="/series/" className="button">View all Series</Link>
-          </label>
-        </div>
-
       </div>
     )
   }
 
+}
+
+function getFacets(results) {
+
+  let type = new Map()
+  let subject = new Map()
+  let creator = new Map()
+  let decade = new Map()
+
+  for (const r of results) {
+    type.set(r.type, type.has(r.type) ? type.get(r.type) + 1 : 1)
+    tally(r.subject, subject)
+    tally(r.creator, creator)
+    tallyDecade(r, decade)
+  }
+
+  type = sortMap(type, results.length).slice(0, 10)
+  subject = sortMap(subject, results.length).slice(0, 10)
+  creator = sortMap(creator, results.length).slice(0, 10)
+  decade = sortDecade(decade, results.length).slice(0, 10)
+
+  return [
+    {name: 'type', counts: type},
+    {name: 'subject', counts: subject},
+    {name: 'creator', counts: creator},
+    {name: 'decade', counts: decade}
+  ]
+
+}
+
+function tally(values, map) {
+  if (values) {
+    for (let v of values) {
+      map.set(v.name, map.has(v.name) ? map.get(v.name) + 1 : 1)
+    }
+  }
+}
+
+function tallyDecade(r, map) {
+  const date = r.type === 'document' ? r.date : r.year
+  if (! date) return
+
+  // a hack to accomodate "1931", "1931-05-23" and "May 1931"
+  const match = date.match(/\d{4}/) 
+  if (! match) return
+
+  const decadeStart = Math.floor(match[0] / 10) * 10
+  if (decadeStart == NaN) return
+
+  const decade = `${decadeStart}-${decadeStart + 9}`
+  if (decade == 'NaN-NaN') {
+    console.log(r)
+  }
+  map.set(decade, map.has(decade) ? map.get(decade) + 1 : 1)
+}
+
+function sortMap(map, total) {
+  return [['All', total], ...map.entries()].sort((a, b) => b[1] - a[1])
+}
+
+function sortDecade(map, total) {
+  return [['All', total], ...map.entries()].sort((a, b) => a[0] < b[0])
 }
 
 export default SearchFacets
--- src/components/search.js
+++ src/components/search.js
@@ -9,27 +9,22 @@ class Search extends Component {
     super(props)
     this.state = {
       category: 'all',
-      query: '',
-      results: []
+      results: [],
+      numResults: 100 
     }
 
-    this.setQuery = this.setQuery.bind(this)
+    this.query = React.createRef()
     this.checkForEnter = this.checkForEnter.bind(this)
     this.setCategory = this.setCategory.bind(this)
     this.setResults = this.setResults.bind(this)
-  }
-
-  componentDidMount() {
-    const query = window.location.hash.substr(1) || ''
-    this.setState({query})
   }
 
   render() {
 
     const ResultList = () => {
       if (this.state.results.length > 0) {
-        return this.state.results.map((item, i) => {
-          return <SearchResult item={item} query={this.state.query} key={`result-${i}`} />
+        return this.state.results.slice(0, this.state.numResults).map((item, i) => {
+          return <SearchResult item={item} query={this.query.current.value} key={`result-${i}`} />
         })
       } else {
         return ''
@@ -40,14 +35,13 @@ class Search extends Component {
       <div className="search columns col_1_3">
 
         <input
+          ref={this.query}
           type="text"
-          value={this.state.query}
-          onChange={this.setQuery}
           onKeyPress={this.checkForEnter}
           placeholder={'Search'}
         />
 
-        <SearchFacets />
+        <SearchFacets results={this.state.results} />
 
         <div className="results">
 		      <div className="facet-panel item-sort">[sorting stuff here]</div>
@@ -60,18 +54,15 @@ class Search extends Component {
 	  )
   }
 
-  setQuery(event) {
-    this.setState({ query: event.target.value })
-  }
-
   checkForEnter(event) {
     if (event.key === 'Enter') {
       this.setResults()
+      event.preventDefault()
     }
   }
 
   setCategory(event) {
-    const query = this.state.query
+    const query = this.query.current.value
     const newCategory = event.target.value
     this.setState({
       category: newCategory,
@@ -81,33 +72,33 @@ class Search extends Component {
 
   setResults() {
     this.setState({
-      results: this.search(this.state.query, this.state.category)
+      results: this.search(this.query.current.value, this.state.category)
     })
   }
 
   getFullResult(r) {
     if (r.id[0] === 'd') {
       const [docId, page] = r.id.split('-')
-      const doc = {...window.__DOCUMENTS__.get(docId)}
-      doc['page'] = page
-      doc['text'] = r['text']
-      return doc
+      const result = {...window.__DOCUMENTS__.get(docId), type: 'Document'}
+      result['page'] = page
+      result['text'] = r['text']
+      return result
     } else if (r.id[0] === 'e') {
-      return {...window.__EPISODES__.get(r.id)}
+      return {...window.__EPISODES__.get(r.id), type: 'Episode'}
     }
   }
 
   search(query, category) {
     if (category === 'episodes') {
-      return window.__INDEX__.search(query, {limit: 100}).filter(r => r.id[0] === 'e').map(r => {
+      return window.__INDEX__.search(query).filter(r => r.id[0] === 'e').map(r => {
         return this.getFullResult(r)
       })
     } else if (category === 'documents') {
-      return window.__INDEX__.search({field: 'text', query: query}, {field: 'title', query: query, limit: 100}).filter(r => r.id[0] === 'd').map(r => {
+      return window.__INDEX__.search({field: 'text', query: query}, {field: 'title', query: query}).filter(r => r.id[0] === 'd').map(r => {
         return this.getFullResult(r)
       })
     } else if (category === 'all') {
-      return window.__INDEX__.search({field: ['text', 'title', 'description'], query: query, bool: 'or', limit: 100}).map(r => {
+      return window.__INDEX__.search({field: ['text', 'title', 'description'], query: query, bool: 'or'}).map(r => {
         return this.getFullResult(r)
       })
     } else {
