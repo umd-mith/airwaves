539e56be1d5277bdcf69a6a2f2ee5d2eee511e11
Kirsten Keister <kkeister@LIBRLTSM250181.fios-router.home>
2019-11-04 19:02:40 -0500

    materials page layout and styling; trying out materials-ui ExpansionPanel

--- src/pages/materials.js
+++ src/pages/materials.js
@@ -4,34 +4,20 @@ import Layout from "../components/layout"
 
 const MaterialsPage = () => (
   <Layout>
-    <h1>Materials</h1>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    mollit anim id est laborum.
-    </p>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    mollit anim id est laborum.
-    </p>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    mollit anim id est laborum.
-    </p>
+  <div className="page-materials">
+    <section className="leader">
+      <h1>Explore Related Materials</h1>
+      <article>
+        <p>The data below is from the NAEB paper collection, housed at the <a href="https://www.wisconsinhistory.org/" title="Wisconsin Historical Society website" target="_blank">Wisconsin Historical Society</a> on the campus of the <a href="https://www.wisc.edu/" title="University of Wisconsin-Madison website" target="_blank">University of Wisconsin-Madison</a>. The NAEB papers mainly cover the period from 1950&ndash;1970, and include correspondence, scripts, reports, promotional materials, speeches of long-time president William G. Harley, ﬁles of the Office of Research and Development and of National Educational Radio (a division of the NAEB), newsletters and other publications. Below you can access the ﬁnding aid for the collection, which contains important scope and content notes, notes about the collection’s provenance, and most importantly, the Contents List. The Contents, or what is in the boxes and folders themselves, are  organized by what are called archival "series,&rdquo; which, according to the Society of American Archivists, are &ldquo;ﬁle units or documents arranged in accordance with a ﬁling system or maintained as a unit because they relate to a particular subject or function, result from the same activity, have a particular form, or because of some other relationship arising out of their creation, receipt, or use.&rdquo;</p>
+  <p>When you expand an arrow next to a folder you’ll see a link to the digitized paper materials. These will launch in a new browser tab. If you want to move on and export a different folder, close the browser tab and return to this page.</p>
+      </article>
+    </section>
+    <section className="columns col_full">
+      <article>
+        <p>Expandable List Here</p>
+      </article>
+    </section>
+  </div>
   </Layout>
 )
 
