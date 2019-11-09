b323de29164da3352a938b97882cf7624f8faf4f
Ed Summers <ehs@pobox.com>
2019-11-09 12:58:00 -0500

    make carousel images smaller so they load faster

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,46 +7,46 @@ import 'slick-carousel/slick/slick-theme.css'
 import './index.css'
 
 const sliderSettings = {
-  className: "carousel slider variable-width",
+  className: "carousel",
   centerMode: true,
-  centerPadding: "60px",
+  centerPadding: "120px",
   dots: true,
   infinite: true,
   speed: 500,
   slidesToShow: 3,
-  slidesToScroll: 3,
-  variableWidth: true
+  variableWidth: true,
+  draggable: true
 }
 
 const IndexPage = () => (
   
   <Layout>
-  <div className="page-home">
-    <section className="leader">
-      <article>
-        <Slider {...sliderSettings}>
-          <div>
-            <img height="200" src={withPrefix("/images/carousel/01.jpg")} />
-          </div>
-          <div>
-            <img height="200" src={withPrefix("/images/carousel/02.jpg")} />
-          </div>
-          <div>
-            <img height="200" src={withPrefix("/images/carousel/03.jpg")} />
-          </div>
-          <div>
-            <img height="200" src={withPrefix("/images/carousel/04.jpg")} />
-          </div>
-          <div>
-            <img height="200" src={withPrefix("/images/carousel/05.jpg")} />
-          </div>
-          <div>
-            <img height="200" src={withPrefix("/images/carousel/06.jpg")} />
-          </div>
-        </Slider>
-      </article>
-    </section>
-  </div>
+    <div className="page-home">
+      <section className="leader">
+        <article>
+          <Slider {...sliderSettings}>
+            <div>
+              <img height="200" src={withPrefix("/images/carousel/01.jpg")} />
+            </div>
+            <div>
+              <img height="200" src={withPrefix("/images/carousel/02.jpg")} />
+            </div>
+            <div>
+              <img height="200" src={withPrefix("/images/carousel/03.jpg")} />
+            </div>
+            <div>
+              <img height="200" src={withPrefix("/images/carousel/04.jpg")} />
+            </div>
+            <div>
+              <img height="200" src={withPrefix("/images/carousel/05.jpg")} />
+            </div>
+            <div>
+              <img height="200" src={withPrefix("/images/carousel/06.jpg")} />
+            </div>
+          </Slider>
+        </article>
+      </section>
+    </div>
   </Layout>
 )
 
