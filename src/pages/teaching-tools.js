08a2b2485a18475a16692306a1488fc7b85f8eb9
Kirsten Keister <kkeister@LIBRLTSM250181.fios-router.home>
2019-11-11 13:29:55 -0500

    basic layouts for About and Teaching pages

--- src/pages/teaching-tools.js
+++ src/pages/teaching-tools.js
@@ -4,37 +4,13 @@ import Layout from "../components/layout"
 
 const TeachingToolsPage = () => (
   <Layout>
-    <div className="page-exhibits">
+    <div className="page-teaching-tools">
       <section className="leader">
         <h1>Teaching Tools</h1>
         <article>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
-          </p>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
-          </p>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
-          </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus cursus massa et felis laoreet mattis sed sit amet lorem. Duis sed mauris et diam fringilla aliquet vitae non ex. Curabitur eu elit porta, faucibus risus non, egestas augue. Praesent nec porta risus. Cras non dolor id velit commodo facilisis. Integer egestas dui quis arcu placerat, et feugiat arcu ultrices. Etiam feugiat sagittis luctus. Donec vel tortor ligula. In pharetra vitae eros eu feugiat. Vivamus vitae sollicitudin lorem. Suspendisse potenti. Mauris luctus mauris ac vulputate mollis.</p>
+          <p>Suspendisse faucibus, magna posuere feugiat ultricies, est justo sodales ligula, et lobortis tellus augue non arcu. Donec ac leo mollis, viverra lectus nec, aliquet velit. Nam eleifend ante sed justo tristique dignissim. Donec auctor ultrices dolor sit amet porta. Aenean varius finibus lectus, semper placerat sem tempor et. Vivamus ultricies tellus non suscipit ultricies. Phasellus nec vulputate mauris. Aliquam dictum enim ut vestibulum consequat. Aliquam vitae risus eget nibh fringilla semper. Nullam ac tincidunt diam, vel sagittis nibh. Nam et leo aliquet, vehicula urna eget, ultricies eros. Donec porta, nisl sit amet hendrerit posuere, massa ligula euismod turpis, non sodales metus urna sed magna.</p>
+          <p>Praesent pellentesque luctus metus sit amet posuere. Vestibulum interdum tellus convallis eros feugiat varius. Proin sollicitudin urna eleifend velit congue, a lacinia ante molestie. Ut sed ullamcorper tellus, sed venenatis leo. Aenean quis ligula non quam posuere vulputate sed in massa. Duis euismod pharetra lorem. Morbi vel velit sit amet purus fermentum pellentesque. Maecenas neque purus, suscipit ut placerat ut, sagittis accumsan dui. Nunc tempor augue eu est dapibus, a molestie augue rutrum. Maecenas quam augue, vestibulum eget lacinia eget, tempor id tellus. Cras egestas vehicula pulvinar. Sed ultricies pretium vehicula. Nullam et lacinia ante. Proin lobortis sapien nunc, consectetur rutrum tellus eleifend nec. Suspendisse nec ipsum dolor.</p>
         </article>
       </section>
     </div>
