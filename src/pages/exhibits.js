fe5b2cb7515751dbc2100f2e0b7cf70ba05dc0c8
Kirsten Keister <kkeister@LIBRLTSM250181.fios-router.home>
2019-11-11 13:30:35 -0500

    2+1 column example layout for exhibits page; add column css to layouts.css

--- src/pages/exhibits.js
+++ src/pages/exhibits.js
@@ -8,33 +8,17 @@ const ExhibitsPage = () => (
       <section className="leader">
         <h1>Exhibits</h1>
         <article>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
-          </p>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
-          </p>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
-          </p>
+          <p>Praesent pellentesque luctus metus sit amet posuere. Vestibulum interdum tellus convallis eros feugiat varius. Proin sollicitudin urna eleifend velit congue, a lacinia ante molestie. Ut sed ullamcorper tellus, sed venenatis leo. Aenean quis ligula non quam posuere vulputate sed in massa. Duis euismod pharetra lorem. Morbi vel velit sit amet purus fermentum pellentesque. Maecenas neque purus, suscipit ut placerat ut, sagittis accumsan dui. Nunc tempor augue eu est dapibus, a molestie augue rutrum. Maecenas quam augue, vestibulum eget lacinia eget, tempor id tellus. Cras egestas vehicula pulvinar. Sed ultricies pretium vehicula. Nullam et lacinia ante. Proin lobortis sapien nunc, consectetur rutrum tellus eleifend nec. Suspendisse nec ipsum dolor.</p>
+        </article>
+      </section>
+      <section className="columns col_1_3">
+        <article className="col_main">
+          <h2>Main Section</h2>
+          <p>Main section content goes here and spans 2 columns.</p>
+        </article>
+        <article className="col_sidebar">
+          <h2>Sidebar Area</h2>
+          <p>Sidebar area content goes here and spans 1 column.</p>
         </article>
       </section>
     </div>
