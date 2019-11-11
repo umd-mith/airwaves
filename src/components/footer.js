e8abb7ac0e906a73d14ee57ae2ed6b7b00261327
Kirsten Keister <kkeister@LIBRLTSM250181.fios-router.home>
2019-11-11 16:39:34 -0500

    adjust style of footer; add NEH logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Link, withPrefix } from "gatsby"
+import { Link } from "gatsby"
 import './footer.css'
 
+import mith from '../images/logo_mith.svg'
+import whs from '../images/logo_whs.svg'
+import uwm from '../images/logo_uw.svg'
+import neh from '../images/logo_neh.svg'
+
 const Footer = () => (
-  <footer id="footer" className="columns">
-    <Link to="https://www.umd.edu/"><img src={withPrefix("../images/logo_mith.svg")} alt="Maryland Institute for Technology in the Humanities (MITH)" /></Link>
-    <Link to="https://mith.umd.edu/"><img src={withPrefix("../images/logo_umd.svg")} alt="University of Maryland" /></Link>
-    <Link to="https://www.wisconsinhistory.org/"><img src={withPrefix("../images/logo_whs.jpg")} alt="Wisconsin Historical Society" /></Link>
-    <Link to="https://www.wisc.edu/"><img src={withPrefix("../images/logo_uw.svg")} alt="University of Wisconsin-Madison" /></Link>
+  <footer>
+    <section>
+      <article  id="footer_logos" className="columns">
+        <Link to="https://mith.umd.edu/"><img src={mith} alt="University of Maryland" /></Link>
+        <Link to="https://www.wisconsinhistory.org/"><img src={whs} alt="Wisconsin Historical Society" /></Link>
+        <Link to="https://www.wisc.edu/"><img src={uwm} alt="University of Wisconsin-Madison" /></Link>
+        <Link to="https://www.neh.gov/"><img src={neh} alt="National Endowment for the Humanities (NEH)" /></Link>
+      </article>
+      <article>
+        <p>Unlocking the Airwaves has been made possible in part by a major grant from the National Endowment for the Humanities' Humanities Collections and Reference Resources (HCRR) program.</p>
+      </article>
+    </section>
   </footer>
 )
 
