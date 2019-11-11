920f20f64a7a22466598b7ba723be86bdaaf9b27
Kirsten Keister <kkeister@LIBRLTSM250181.fios-router.home>
2019-11-11 16:04:37 -0500

    adding footer layout and styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link, withPrefix } from "gatsby"
 import './footer.css'
 
 const Footer = () => (
-  <footer>
-    <a href="https://mith.umd.edu">MITH</a>
+  <footer id="footer" className="columns">
+    <Link to="https://www.umd.edu/"><img src={withPrefix("../images/logo_mith.svg")} alt="Maryland Institute for Technology in the Humanities (MITH)" /></Link>
+    <Link to="https://mith.umd.edu/"><img src={withPrefix("../images/logo_umd.svg")} alt="University of Maryland" /></Link>
+    <Link to="https://www.wisconsinhistory.org/"><img src={withPrefix("../images/logo_whs.jpg")} alt="Wisconsin Historical Society" /></Link>
+    <Link to="https://www.wisc.edu/"><img src={withPrefix("../images/logo_uw.svg")} alt="University of Wisconsin-Madison" /></Link>
   </footer>
 )
 
