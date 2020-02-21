8d59461d46bd466ea279f70302e58b573daf04eb
Ed Summers <ehs@pobox.com>
2020-02-21 19:40:47 +0000

    removed unused libvips related dependencies to avoid gcc

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,6 @@ module.exports = {
       }
     },
     `gatsby-transformer-json`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
