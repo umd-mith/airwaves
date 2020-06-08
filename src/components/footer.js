8f893ac2f4bf60a3546b6d3e17b662d32e33a437
kirstenkeister <kirsten.keister@gmail.com>
2020-06-08 13:25:49 -0400

    make footer logos responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,15 +7,15 @@ import UWM from '../svg/logo_uw.svg'
 import NEH from '../svg/logo_neh.svg'
 
 const Footer = () => (
-  <footer>j
+  <footer>
     <section>
-      <article id="footer_logos" className="columns">
-        <a href="https://mith.umd.edu/"> <MITH /> </a>
-        <a href="https://www.wisconsinhistory.org/"> <WHS /> </a>
-        <a href="https://www.wisc.edu/"> <UWM /> </a>
-        <a href="https://www.neh.gov/"> <NEH /> </a>
+      <article className="columns">
+        <a className="col-3 col-6-md col-6-sm col-12-xs" href="https://mith.umd.edu/"> <MITH /> </a>
+        <a className="col-3 col-6-md col-6-sm col-12-xs" href="https://www.wisconsinhistory.org/"> <WHS /> </a>
+        <a className="col-3 col-6-md col-6-sm col-12-xs" href="https://www.wisc.edu/"> <UWM /> </a>
+        <a className="col-3 col-6-md col-6-sm col-12-xs" href="https://www.neh.gov/"> <NEH /> </a>
       </article>
-      <article>
+      <article className="disclaimer">
         <p>Unlocking the Airwaves has been made possible in part by a major grant from the National Endowment for the Humanities' Humanities Collections and Reference Resources (HCRR) program.</p>
       </article>
     </section>
