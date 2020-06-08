3962138c59c45d911b4839206c98afb7c62334cc
kirstenkeister <kirsten.keister@gmail.com>
2020-06-08 13:25:06 -0400

    add CSS Grid styles to pages & templates

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -10,28 +10,22 @@ const ExplorePage = () => (
       <h1>Explore the Archive</h1>
       <article>
         <p>The Search page is a way you can explore the entire Unlocking the Airwaves collection, including NAEB radio programs and related archival materials, from one point of entry. You may either browse the collection using what are called ‘facets’ (pre-defined categories and queries) on the left side of the screen, or you can conduct an Advanced Search of the whole collection using keywords, names of people or organizations, geographic regions, subjects, or time periods. The Search page is broadly useful to find a wide variety of materials in the collections, but if you’d prefer a more streamlined approach, you may also browse the collections by either radio programs (by series/episode), or within the series of folders from the Wisconsin Historical Society’s NAEB paper collections. Read below for more on how to explore by radio program or related materials.</p>
-        <Link className="button" to="/search/">
-        Search the Archive
-        </Link>
+        <Link className="button" to="/search/">Search the Archive</Link>
       </article>
     </section>
-    <section className="columns col_1_2">
-      <article>
+    <section className="columns">
+      <article className="col-6 col-12-xs">
         <h2>Explore Radio Programs</h2>
         <p>The radio programming service of the NAEB, known as the National Educational Radio Network (NERN), comprised broadcasts which balanced a focus on public service reporting of national events (e.g., the economy, the depression, World War II, the Civil Rights movement) with providing a forum for local issues (e.g., agricultural prices, community events, elections) and with expanding public education.</p>
         <p>These broadcasts, mostly stemming from university and public school-run radio stations, provide an in-depth look at the engagements and events of American history, as they were broadcast to and received by the general public in the twentieth century. They cover crucial educational, political, and cultural events as diverse as the national census, atomic energy, American labor, religion, United States history, agricultural engineering, mathematics, and foreign relations.</p>
         <p>Click below to browse the NERN broadcasts by series title.</p>
-        <Link to="/programs/" className="button">
-        Explore Programs
-        </Link>
+        <Link to="/programs/" className="button">Explore Programs</Link>
       </article>
-      <article>
+      <article className="col-6 col-12-xs">
         <h2>Explore Related Materials</h2>
         <p>Click the link below to browse materials from the NAEB paper collection, housed at the Wisconsin Historical Society on the campus of the University of Wisconsin-Madison. The NAEB papers mainly cover the period from 1950–1970, and include correspondence, scripts, reports, promotional materials, speeches of long-time president William G. Harley, ﬁles of the Office of Research and Development and of National Educational Radio (a division of the NAEB), newsletters and other publications. Below you can access a version of the WHS ﬁnding aid for the collection, which contains important scope and content notes, notes about the collection’s provenance, and most importantly, the Contents List. The Contents, or what is in the boxes and folders themselves, are organized by what are called archival "series.”</p>
         <p><b>Beta Launch Note</b> Right now the only folder which users can link to from this interface is under Contents List > Subject Files > Box 34 Folder 4: Public Relations, 1954-1965. If the advisors deem this interface useful, we will proceed with including the rest of the digitized folders in this interface.</p>
-        <Link to="/materials/" className="button">
-        Explore Materials
-        </Link>
+        <Link to="/materials/" className="button">Explore Materials</Link>
       </article>
     </section>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link, withPrefix } from "gatsby"
+import React from 'react'
+import { Link, withPrefix } from 'gatsby'
 import Layout from "../components/layout"
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
@@ -58,33 +58,33 @@ const IndexPage = () => (
           </Slider>
         </article>
       </section>
-      <section className="columns col_1_3">
-        <article className="col_main">
+      <section className="columns">
+        <article className="col-7 col-5-sm col-10-xs">
           <h2>Unlocking the Airwaves</h2>
           <p>Unlocking the Airwaves: Revitalizing an Early Public and Educational Radio Collection is a comprehensive online collection of early educational public radio content from the National Association of Educational Broadcasters (NAEB). The forerunner of CPB and its arms, NPR and PBS, the NAEB developed and distributed educational radio programs and accompanying print materials to schools and communities across the United States. What’s more, the NAEB lobbied extensively to unlock the airwaves—to access precious frequency space—in order to bring the voices of poet Robert Frost, architect Frank Lloyd Wright, anthropologist Margaret Mead, and conservationist “Ranger Mac,” among many other individuals, into American homes and classrooms.</p>
           <p>The NAEB’s history is the dramatic story of idealists who believed in the utopian possibilities of technology for education and social uplift and who faced considerable challenges in pursuit of those goals, including economic depression, world war, and the scarcity of the electromagnetic spectrum. It’s a story that has much to tell us about 20th century American culture, as well as the 21st century’s environment of online educational technology and podcasting that we live in today.</p>
           <p>Despite its historic importance and contemporary relevance, most of the NAEB members’ programs were never heard again after their initial brief moments on the air. The archives for the radio programs and their related paper documentation have been split for over 25 years between two institutions: the University of Maryland and the Wisconsin Historical Society. Unlocking the Airwaves reunites the split collections, ﬁnally realizing the potential of the collections of the NAEB for exploration and and the broader public.</p>
-          <div className="columns col_1_2">
-            <Link to="/about/" className="button">Read More about the Project</Link>
-            <Link to="/explore/" className="button">Explore the Archive</Link>
+          <div className="columns">
+            <Link to="/about/" className="col-6 col-12-xs button">Read More about the Project</Link>
+            <Link to="/explore/" className="col-6 col-12-xs button">Explore the Archive</Link>
           </div>                          
         </article>
-        <article id="featured" className="col_sidebar">
+        <article id="featured" className="col-3 col-5-sm col-10-xs">
           <h2>Featured Exhibits</h2>
           <div>
             <Link to="/" style={{ color: "#444" }}>
-              <p className="columns col_1_2">
-                <img alt="Cold War 1" width="100%" src={withPrefix("/images/exhibits/ex_cold-war1.jpg")} />
-                <img alt="Cold War 2" width="100%" src={withPrefix("/images/exhibits/ex_cold-war2.jpg")} />
+              <p className="columns">
+                <img className="col-6 col-12-xs" alt="Cold War 1" width="100%" src={withPrefix("/images/exhibits/ex_cold-war1.jpg")} />
+                <img className="col-6 col-12-xs" alt="Cold War 2" width="100%" src={withPrefix("/images/exhibits/ex_cold-war2.jpg")} />
               </p>
               <span className="caption">Radio During the Cold War</span>
             </Link>
           </div>
           <div>
             <Link to="/" style={{ color: "#444" }}>
-              <p className="columns col_1_2">
-                <img alt="Jazz 1" width="100%" src={withPrefix("/images/exhibits/ex-american-jazz1.jpg")} />
-                <img alt="Jazz 2" width="100%" src={withPrefix("/images/exhibits/ex-american-jazz2.jpg")} />
+              <p className="columns">
+                <img className="col-6 col-12-xs" alt="Jazz 1" width="100%" src={withPrefix("/images/exhibits/ex-american-jazz1.jpg")} />
+                <img className="col-6 col-12-xs" alt="Jazz 2" width="100%" src={withPrefix("/images/exhibits/ex-american-jazz2.jpg")} />
               </p>
               <span className="caption">American Jazz in the 60s</span>
             </Link>
