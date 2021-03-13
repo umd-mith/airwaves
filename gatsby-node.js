umd-mith/airwaves

commit 394ded5daca2d1c56a7d42f6b0b710c546e72e17

siteUrl includes trailing slash so RSS urls were invalid
Not sure if this is new behavior of path due to #2 or just happened to notice now

gatsby-node.js
@@ -172,9 +172,9 @@ async function rss(graphql, force=false) {
 
   results.data.allSeriesJson.nodes.forEach(series => {
     const feedPath = `./static/rss/${series.id}.xml`
-    const imageUrl = `${siteUrl}/images/podcast.png`
-    const feedUrl = `${siteUrl}/rss/${series.id}.xml`
-    const seriesUrl = `${siteUrl}/programs/${series.id}/`
+    const imageUrl = `${siteUrl}images/podcast.png`
+    const feedUrl = `${siteUrl}rss/${series.id}.xml`
+    const seriesUrl = `${siteUrl}programs/${series.id}/`
 
     // only write the RSS file if it's not there already
     // or we have been asked to force a rewrite.

commit a78e91a9e8e079b6fc0a6e82e27fc9beff838208

Fixes regression introduced in 394ded5

gatsby-node.js
@@ -172,9 +172,9 @@ async function rss(graphql, force=false) {
 
   results.data.allSeriesJson.nodes.forEach(series => {
     const feedPath = `./static/rss/${series.id}.xml`
-    const imageUrl = `${siteUrl}images/podcast.png`
-    const feedUrl = `${siteUrl}rss/${series.id}.xml`
-    const seriesUrl = `${siteUrl}programs/${series.id}/`
+    const imageUrl = `${siteUrl}/images/podcast.png`
+    const feedUrl = `${siteUrl}/rss/${series.id}.xml`
+    const seriesUrl = `${siteUrl}/programs/${series.id}/`
 
     // only write the RSS file if it's not there already
     // or we have been asked to force a rewrite.

static/rss/programs.opml
@@ -4,7 +4,6 @@
     <title>Unlocking the Airwaves Podcasts</title>
   </head>
   <body>    <outline type="rss" text="Ask President Carter" title="Ask President Carter" xmlUrl="https://mith.umd.edu/airwaves/rss/ask-president-carter.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/ask-president-carter/"/>
-    <outline type="rss" text="The Jeffersonian heritage" title="The Jeffersonian heritage" xmlUrl="https://mith.umd.edu/airwaves/rss/the-jeffersonian-heritage.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-jeffersonian-heritage/"/>
     <outline type="rss" text="Donald P. Kent memorial lecture" title="Donald P. Kent memorial lecture" xmlUrl="https://mith.umd.edu/airwaves/rss/donald-p-kent-memorial-lecture.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/donald-p-kent-memorial-lecture/"/>
     <outline type="rss" text="Firing Line" title="Firing Line" xmlUrl="https://mith.umd.edu/airwaves/rss/firing-line.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/firing-line/"/>
     <outline type="rss" text="Beethoven: The man who freed music" title="Beethoven: The man who freed music" xmlUrl="https://mith.umd.edu/airwaves/rss/beethoven-the-man-who-freed-music.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/beethoven-the-man-who-freed-music/"/>
@@ -55,7 +54,6 @@
     <outline type="rss" text="Ernest Bloch: The man and his music" title="Ernest Bloch: The man and his music" xmlUrl="https://mith.umd.edu/airwaves/rss/ernest-bloch-the-man-and-his-music.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/ernest-bloch-the-man-and-his-music/"/>
     <outline type="rss" text="Special of the week" title="Special of the week" xmlUrl="https://mith.umd.edu/airwaves/rss/special-of-the-week.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/special-of-the-week/"/>
     <outline type="rss" text="The Institute on Man and Science" title="The Institute on Man and Science" xmlUrl="https://mith.umd.edu/airwaves/rss/the-institute-on-man-and-science.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-institute-on-man-and-science/"/>
-    <outline type="rss" text="Cincinnati Symphony Orchestra" title="Cincinnati Symphony Orchestra" xmlUrl="https://mith.umd.edu/airwaves/rss/cincinnati-symphony-orchestra.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/cincinnati-symphony-orchestra/"/>
     <outline type="rss" text="Metaphysical roots of the drama" title="Metaphysical roots of the drama" xmlUrl="https://mith.umd.edu/airwaves/rss/metaphysical-roots-of-the-drama.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/metaphysical-roots-of-the-drama/"/>
     <outline type="rss" text="Assembly for Human Rights: Montreal" title="Assembly for Human Rights: Montreal" xmlUrl="https://mith.umd.edu/airwaves/rss/assembly-for-human-rights-montreal.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/assembly-for-human-rights-montreal/"/>
     <outline type="rss" text="The smoking dilemma" title="The smoking dilemma" xmlUrl="https://mith.umd.edu/airwaves/rss/the-smoking-dilemma.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-smoking-dilemma/"/>
@@ -123,7 +121,6 @@
     <outline type="rss" text="The Middle East War" title="The Middle East War" xmlUrl="https://mith.umd.edu/airwaves/rss/the-middle-east-war.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-middle-east-war/"/>
     <outline type="rss" text="In memoriam: Langston Hughes" title="In memoriam: Langston Hughes" xmlUrl="https://mith.umd.edu/airwaves/rss/in-memoriam-langston-hughes.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/in-memoriam-langston-hughes/"/>
     <outline type="rss" text="Public Broadcasting Act of 1967" title="Public Broadcasting Act of 1967" xmlUrl="https://mith.umd.edu/airwaves/rss/public-broadcasting-act-of-1967.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/public-broadcasting-act-of-1967/"/>
-    <outline type="rss" text="Improvisation Chamber Ensemble" title="Improvisation Chamber Ensemble" xmlUrl="https://mith.umd.edu/airwaves/rss/improvisation-chamber-ensemble.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/improvisation-chamber-ensemble/"/>
     <outline type="rss" text="Middle East crisis" title="Middle East crisis" xmlUrl="https://mith.umd.edu/airwaves/rss/middle-east-crisis.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/middle-east-crisis/"/>
     <outline type="rss" text="Canada &apos;67" title="Canada &apos;67" xmlUrl="https://mith.umd.edu/airwaves/rss/canada-67.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/canada-67/"/>
     <outline type="rss" text="Conversations on public relations" title="Conversations on public relations" xmlUrl="https://mith.umd.edu/airwaves/rss/conversations-on-public-relations.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/conversations-on-public-relations/"/>
@@ -143,7 +140,6 @@
     <outline type="rss" text="Music around the world" title="Music around the world" xmlUrl="https://mith.umd.edu/airwaves/rss/music-around-the-world.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/music-around-the-world/"/>
     <outline type="rss" text="The piano sonatas of Haydn" title="The piano sonatas of Haydn" xmlUrl="https://mith.umd.edu/airwaves/rss/the-piano-sonatas-of-haydn.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-piano-sonatas-of-haydn/"/>
     <outline type="rss" text="The march of medicine" title="The march of medicine" xmlUrl="https://mith.umd.edu/airwaves/rss/the-march-of-medicine.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-march-of-medicine/"/>
-    <outline type="rss" text="Detroit Symphony Orchestra" title="Detroit Symphony Orchestra" xmlUrl="https://mith.umd.edu/airwaves/rss/detroit-symphony-orchestra.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/detroit-symphony-orchestra/"/>
     <outline type="rss" text="Hall of song: The &apos;Met,&apos; 1883-1966" title="Hall of song: The &apos;Met,&apos; 1883-1966" xmlUrl="https://mith.umd.edu/airwaves/rss/hall-of-song-the-met-1883-1966.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/hall-of-song-the-met-1883-1966/"/>
     <outline type="rss" text="Bluegrass and mountains" title="Bluegrass and mountains" xmlUrl="https://mith.umd.edu/airwaves/rss/bluegrass-and-mountains.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/bluegrass-and-mountains/"/>
     <outline type="rss" text="World of the Paperback" title="World of the Paperback" xmlUrl="https://mith.umd.edu/airwaves/rss/world-of-the-paperback.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/world-of-the-paperback/"/>
@@ -177,7 +173,6 @@
     <outline type="rss" text="The Music of Don Gillis II" title="The Music of Don Gillis II" xmlUrl="https://mith.umd.edu/airwaves/rss/the-music-of-don-gillis-ii.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-music-of-don-gillis-ii/"/>
     <outline type="rss" text="Exploring the child&apos;s world III" title="Exploring the child&apos;s world III" xmlUrl="https://mith.umd.edu/airwaves/rss/exploring-the-childs-world-iii.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/exploring-the-childs-world-iii/"/>
     <outline type="rss" text="The yankee dollar" title="The yankee dollar" xmlUrl="https://mith.umd.edu/airwaves/rss/the-yankee-dollar.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-yankee-dollar/"/>
-    <outline type="rss" text="Ways of mankind" title="Ways of mankind" xmlUrl="https://mith.umd.edu/airwaves/rss/ways-of-mankind.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/ways-of-mankind/"/>
     <outline type="rss" text="Smoke?" title="Smoke?" xmlUrl="https://mith.umd.edu/airwaves/rss/smoke.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/smoke/"/>
     <outline type="rss" text="They bent our ear" title="They bent our ear" xmlUrl="https://mith.umd.edu/airwaves/rss/they-bent-our-ear.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/they-bent-our-ear/"/>
     <outline type="rss" text="America on stage" title="America on stage" xmlUrl="https://mith.umd.edu/airwaves/rss/america-on-stage.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/america-on-stage/"/>
@@ -259,7 +254,6 @@
     <outline type="rss" text="Security and civil rights" title="Security and civil rights" xmlUrl="https://mith.umd.edu/airwaves/rss/security-and-civil-rights.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/security-and-civil-rights/"/>
     <outline type="rss" text="Indian country" title="Indian country" xmlUrl="https://mith.umd.edu/airwaves/rss/indian-country.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/indian-country/"/>
     <outline type="rss" text="Politics in the twentieth century" title="Politics in the twentieth century" xmlUrl="https://mith.umd.edu/airwaves/rss/politics-in-the-twentieth-century.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/politics-in-the-twentieth-century/"/>
-    <outline type="rss" text="Peninsula Music Festival" title="Peninsula Music Festival" xmlUrl="https://mith.umd.edu/airwaves/rss/peninsula-music-festival.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/peninsula-music-festival/"/>
     <outline type="rss" text="Listen America" title="Listen America" xmlUrl="https://mith.umd.edu/airwaves/rss/listen-america.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/listen-america/"/>
     <outline type="rss" text="Roots of jazz" title="Roots of jazz" xmlUrl="https://mith.umd.edu/airwaves/rss/roots-of-jazz.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/roots-of-jazz/"/>
     <outline type="rss" text="Window on the world" title="Window on the world" xmlUrl="https://mith.umd.edu/airwaves/rss/window-on-the-world.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/window-on-the-world/"/>
@@ -288,7 +282,6 @@
     <outline type="rss" text="Patrioscript" title="Patrioscript" xmlUrl="https://mith.umd.edu/airwaves/rss/patrioscript.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/patrioscript/"/>
     <outline type="rss" text="People talk back" title="People talk back" xmlUrl="https://mith.umd.edu/airwaves/rss/people-talk-back.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/people-talk-back/"/>
     <outline type="rss" text="Voices of Europe" title="Voices of Europe" xmlUrl="https://mith.umd.edu/airwaves/rss/voices-of-europe.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/voices-of-europe/"/>
-    <outline type="rss" text="People under communism" title="People under communism" xmlUrl="https://mith.umd.edu/airwaves/rss/people-under-communism.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/people-under-communism/"/>
     <outline type="rss" text="Four documentaries" title="Four documentaries" xmlUrl="https://mith.umd.edu/airwaves/rss/four-documentaries.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/four-documentaries/"/>
     <outline type="rss" text="Community education project" title="Community education project" xmlUrl="https://mith.umd.edu/airwaves/rss/community-education-project.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/community-education-project/"/>
     <outline type="rss" text="The urban confrontation" title="The urban confrontation" xmlUrl="https://mith.umd.edu/airwaves/rss/the-urban-confrontation.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-urban-confrontation/"/>
@@ -389,7 +382,6 @@
     <outline type="rss" text="Sexuality: a search for perspective" title="Sexuality: a search for perspective" xmlUrl="https://mith.umd.edu/airwaves/rss/sexuality-a-search-for-perspective.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/sexuality-a-search-for-perspective/"/>
     <outline type="rss" text="The American woman in fact and fiction" title="The American woman in fact and fiction" xmlUrl="https://mith.umd.edu/airwaves/rss/the-american-woman-in-fact-and-fiction.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-american-woman-in-fact-and-fiction/"/>
     <outline type="rss" text="pro and con" title="pro and con" xmlUrl="https://mith.umd.edu/airwaves/rss/pro-and-con.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/pro-and-con/"/>
-    <outline type="rss" text="Library of Congress" title="Library of Congress" xmlUrl="https://mith.umd.edu/airwaves/rss/library-of-congress.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/library-of-congress/"/>
     <outline type="rss" text="Environment in the 70s" title="Environment in the 70s" xmlUrl="https://mith.umd.edu/airwaves/rss/environment-in-the-70s.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/environment-in-the-70s/"/>
     <outline type="rss" text="The future of" title="The future of" xmlUrl="https://mith.umd.edu/airwaves/rss/the-future-of.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/the-future-of/"/>
     <outline type="rss" text="Down to the sea" title="Down to the sea" xmlUrl="https://mith.umd.edu/airwaves/rss/down-to-the-sea.xml" htmlUrl="https://mith.umd.edu/airwaves/programs/down-to-the-sea/"/>