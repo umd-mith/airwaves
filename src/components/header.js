af04315db610893e924241b15991b53c8bd7ce8b
trevormunoz <trevor@trevormunoz.com>
2021-03-26 14:56:55 -0400

    Add simplified logo and sketch of header

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,18 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-// import './header.css'
+import './header.css'
 import Logo from '../svg/uta-header.svg'
+import MobileLogo from '../svg/uta-header-core_only.svg'
 import Nav from './nav'
 
 const Header = () => {
 
     return (
       <header>
-        <Link to="/" className="main-logo">
-          <Logo alt="Unlocking the Airwaves" />
+        <Link to="/">
+          <Logo className="main-logo" alt="Unlocking the Airwaves" />
+          <MobileLogo className="alt-logo" alt="Unlocking the Airwaves" />
         </Link>
         <Nav />
       </header>
