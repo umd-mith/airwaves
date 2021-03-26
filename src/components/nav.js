b51237223c555dd41f3230a2d1a8474d4f9c00ac
trevormunoz <trevor@trevormunoz.com>
2021-03-25 21:03:20 -0400

    Basic nav styles and media queries

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,8 @@
 import { Link, useStaticQuery, graphql } from "gatsby"
 import React from "react"
 
+import './nav.css'
+
 const Nav = () => {
     const navData = useStaticQuery(graphql`
     {
@@ -16,15 +18,15 @@ const Nav = () => {
   `)
 
   return(
-    <div className="main-nav">
+    <nav className="main-nav">
         <ul className="main-nav-menu">
             {navData.site.siteMetadata.siteNav.map(link => (
                 <li key={link.name}>
-                <Link activeClassName="active" to={link.link}>{link.name}</Link>
+                    <Link activeClassName="active" to={link.link}>{link.name}</Link>
                 </li>
             ))}
         </ul>
-    </div>
+    </nav>
   )
 }
 
