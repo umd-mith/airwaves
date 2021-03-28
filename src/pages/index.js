b08b0c87f9f9526fb7d9180dfa3d5830426b5ad0
trevormunoz <trevor@trevormunoz.com>
2021-03-28 15:38:32 -0400

    Get rid of class cruft

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,15 +60,17 @@ const IndexPage = () => (
         <p>Unlocking the Airwaves: Revitalizing an Early Public and Educational Radio Collection is a comprehensive online collection of early educational public radio content from the National Association of Educational Broadcasters (NAEB). The forerunner of CPB and its arms, NPR and PBS, the NAEB developed and distributed educational radio programs and accompanying print materials to schools and communities across the United States. What’s more, the NAEB lobbied extensively to unlock the airwaves—to access precious frequency space—in order to bring the voices of poet Robert Frost, architect Frank Lloyd Wright, anthropologist Margaret Mead, and conservationist “Ranger Mac,” among many other individuals, into American homes and classrooms.</p>
         <p>The NAEB’s history is the dramatic story of idealists who believed in the utopian possibilities of technology for education and social uplift and who faced considerable challenges in pursuit of those goals, including economic depression, world war, and the scarcity of the electromagnetic spectrum. It’s a story that has much to tell us about 20th century American culture, as well as the 21st century’s environment of online educational technology and podcasting that we live in today.</p>
         <p>Despite its historic importance and contemporary relevance, most of the NAEB members’ programs were never heard again after their initial brief moments on the air. The archives for the radio programs and their related paper documentation have been split for over 25 years between two institutions: the University of Maryland and the Wisconsin Historical Society. Unlocking the Airwaves reunites the split collections, ﬁnally realizing the potential of the collections of the NAEB for exploration and and the broader public.</p>
-        <div>
-          <Link to="/about/" className="col-6 col-6-lg col-12-md col-12-sm  col-12-xs button">Read More about the Project</Link>
-          <Link to="/explore/" className="col-6 col-6-lg col-12-md col-12-sm  col-12-xs button">Explore the Archive</Link>
+        <div className="internal-jump-links">
+          <Link to="/about/">Read More about the Project</Link>
+          <Link to="/explore/">Explore the Archive</Link>
         </div>
       </section>                          
       <section>
         <h2>Featured Exhibits</h2>
         {/* Insert real content */}
-        <Link to="/exhibits/" className="button">More Exhibits</Link>                           
+        <div className="internal-jump-links">
+          <Link to="/exhibits/">More Exhibits</Link>
+        </div>                           
       </section>
   </Layout>
 )
