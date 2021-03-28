d430d4d16ec0169bd2a40955d4e4a797a6f8e172
trevormunoz <trevor@trevormunoz.com>
2021-03-28 15:29:36 -0400

    Add menu toggle for mobile

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,33 +1,48 @@
 import { Link, useStaticQuery, graphql } from "gatsby"
-import React from "react"
+import React, { useState } from 'react';
 
 import './nav.css'
 
 const Nav = () => {
     const navData = useStaticQuery(graphql`
-    {
-        site {
-        siteMetadata {
-            siteNav {
-            name
-            link
+        {
+            site {
+            siteMetadata {
+                siteNav {
+                name
+                link
+                }
+            }
             }
         }
-        }
-    }
-  `)
+    `)
+    
+    const [hidden, setHidden] = useState(true);
 
-  return(
-    <nav className="main-nav">
-        <ul className="main-nav-menu">
-            {navData.site.siteMetadata.siteNav.map(link => (
-                <li key={link.name}>
-                    <Link activeClassName="active" to={link.link}>{link.name}</Link>
-                </li>
-            ))}
-        </ul>
-    </nav>
-  )
+    const toggleMenu = () => {
+        setHidden(prevState => !prevState)
+    }
+    
+    return(
+        <div className="main-nav-container">
+            <button 
+                className="header-menu-button" 
+                type="button" 
+                rel="noopener noreferrer"
+                onClick={toggleMenu}>
+                    Menu
+            </button>
+            <nav className="main-nav">
+                <ul id="main-nav-menu" className={hidden ? 'menu': 'menu menu-open'}>
+                    {navData.site.siteMetadata.siteNav.map(link => (
+                        <li key={link.name}>
+                            <Link activeClassName="active" to={link.link}>{link.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Nav
