7cab1f3ac8216387c1a47950064fbb13ff7ea3ed
trevormunoz <trevor@trevormunoz.com>
2021-04-14 20:18:42 -0400

    Refactor ExhibitSummary into reusable component

---
+++ src/components/exhibit-summary.js
@@ -0,0 +1,21 @@
+import path from "path"
+import React from "react"
+import { Link, withPrefix } from "gatsby"
+
+const ExhibitSummaryCard = ({ title, keyImage, lede, absPath }) => {
+  const slug = path.basename(absPath).replace(/\.md$/, "")
+  const url = `/exhibits/${slug}/`
+
+  return (
+    <div className="exhibit-summary-card">
+      <h2>
+        <Link to={url}>{title}</Link>
+      </h2>
+      <img src={withPrefix(keyImage.image)} alt={keyImage.title} />
+      <p>{lede}</p>
+      <Link to={url}>Read More...</Link>
+    </div>
+  )
+}
+
+export default ExhibitSummaryCard
