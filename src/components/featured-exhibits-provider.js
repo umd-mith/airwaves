637b52a309ec50afba37bf2575e5062adc6ca2a0
trevormunoz <trevor@trevormunoz.com>
2021-04-14 20:19:38 -0400

    Create provider component to get ExhibitSummary for only featured exhibits

---
+++ src/components/featured-exhibits-provider.js
@@ -0,0 +1,72 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import ExhibitSummaryCard from "./exhibit-summary"
+
+const FeaturedExhibitsProvider = () => {
+  const exhibitsData = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___title, order: ASC }) {
+        nodes {
+          frontmatter {
+            title
+            lede
+            visuals {
+              image
+              title
+            }
+          }
+          fileAbsolutePath
+        }
+      }
+      exhibitsJson {
+        exhibits_by_title
+      }
+    }
+  `)
+
+  const filterExhibits = data => {
+    const allExhibits = data.allMarkdownRemark.nodes
+    const featuredTitles = data.exhibitsJson.exhibits_by_title
+
+    return allExhibits.filter(item =>
+      featuredTitles.includes(item.frontmatter.title)
+    )
+  }
+
+  const reshapeExhibitData = exhibitObj => {
+    let allImages = exhibitObj.frontmatter.visuals
+    let keyImage = {}
+    if (allImages && allImages.length > 0) {
+      keyImage = {
+        image: allImages[0].image,
+        title: allImages[0].title,
+      }
+    }
+    return {
+      absPath: exhibitObj.fileAbsolutePath,
+      title: exhibitObj.frontmatter.title,
+      lede: exhibitObj.frontmatter.lede,
+      keyImage: keyImage,
+    }
+  }
+
+  const featured = filterExhibits(exhibitsData)
+  const featuredAsProps = featured.map(e => {
+    return reshapeExhibitData(e)
+  })
+
+  return (
+    <>
+      {featuredAsProps.map(cardProps => (
+        <ExhibitSummaryCard
+          title={cardProps.title}
+          keyImage={cardProps.keyImage}
+          lede={cardProps.lede}
+          absPath={cardProps.absPath}
+        />
+      ))}
+    </>
+  )
+}
+
+export default FeaturedExhibitsProvider
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Link, withPrefix } from 'gatsby'
+import React from "react"
+import { Link, withPrefix } from "gatsby"
 import Layout from "../components/layout"
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import FeaturedExhibitsProvider from "../components/featured-exhibits-provider"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 // import './index.css'
 
 const sliderSettings = {
@@ -24,63 +25,103 @@ const sliderSettings = {
         centerPadding: "10px",
         slidesToShow: 1,
         slidesToScroll: 1,
-        infinite: false
-      }
-    }
-  ]
+        infinite: false,
+      },
+    },
+  ],
 }
 
 const IndexPage = () => (
-  
   <Layout>
     <section>
       <Slider {...sliderSettings}>
         <div>
-          <img alt="Document Page" height="200" src={withPrefix("/images/carousel/01.jpg")} />
+          <img
+            alt="Document Page"
+            height="200"
+            src={withPrefix("/images/carousel/01.jpg")}
+          />
         </div>
         <div>
-          <img alt="Document folded" height="200" src={withPrefix("/images/carousel/02.jpg")} />
+          <img
+            alt="Document folded"
+            height="200"
+            src={withPrefix("/images/carousel/02.jpg")}
+          />
         </div>
         <div>
-          <img alt="Folders" height="200" src={withPrefix("/images/carousel/03.jpg")} />
+          <img
+            alt="Folders"
+            height="200"
+            src={withPrefix("/images/carousel/03.jpg")}
+          />
         </div>
         <div>
-          <img alt="The Jeffersonian Heritage Program" height="200" src={withPrefix("/images/carousel/04.jpg")} />
+          <img
+            alt="The Jeffersonian Heritage Program"
+            height="200"
+            src={withPrefix("/images/carousel/04.jpg")}
+          />
         </div>
         <div>
-          <img alt="Ace Tape Program" height="200" src={withPrefix("/images/carousel/05.jpg")} />
+          <img
+            alt="Ace Tape Program"
+            height="200"
+            src={withPrefix("/images/carousel/05.jpg")}
+          />
         </div>
         <div>
-          <img alt="Human Behavior Pamphlet" height="200" src={withPrefix("/images/carousel/06.jpg")} />
+          <img
+            alt="Human Behavior Pamphlet"
+            height="200"
+            src={withPrefix("/images/carousel/06.jpg")}
+          />
         </div>
       </Slider>
     </section>
     <section>
-        <h2>Unlocking the Airwaves</h2>
-        <p>Unlocking the Airwaves: Revitalizing an Early Public and Educational Radio Collection is a comprehensive online collection of early educational public radio content from the National Association of Educational Broadcasters (NAEB). The forerunner of CPB and its arms, NPR and PBS, the NAEB developed and distributed educational radio programs and accompanying print materials to schools and communities across the United States. What’s more, the NAEB lobbied extensively to unlock the airwaves—to access precious frequency space—in order to bring the voices of poet Robert Frost, architect Frank Lloyd Wright, anthropologist Margaret Mead, and conservationist “Ranger Mac,” among many other individuals, into American homes and classrooms.</p>
-        <p>The NAEB’s history is the dramatic story of idealists who believed in the utopian possibilities of technology for education and social uplift and who faced considerable challenges in pursuit of those goals, including economic depression, world war, and the scarcity of the electromagnetic spectrum. It’s a story that has much to tell us about 20th century American culture, as well as the 21st century’s environment of online educational technology and podcasting that we live in today.</p>
-        <p>Despite its historic importance and contemporary relevance, most of the NAEB members’ programs were never heard again after their initial brief moments on the air. The archives for the radio programs and their related paper documentation have been split for over 25 years between two institutions: the University of Maryland and the Wisconsin Historical Society. Unlocking the Airwaves reunites the split collections, ﬁnally realizing the potential of the collections of the NAEB for exploration and and the broader public.</p>
-        <div className="internal-jump-links">
-          <Link to="/about/">Read More about the Project</Link>
-          <Link to="/explore/">Explore the Archive</Link>
-        </div>
-      </section>                          
-      <section>
-        <h2>Featured Exhibits</h2>
-        {/* Insert real content */}
-        <div className="exhibit-summary-card">
-          <img src="https://via.placeholder.com/340x200" />
-        </div>
-        <div className="exhibit-summary-card">
-          <img src="https://via.placeholder.com/340x200" />
-        </div>
-        <div className="exhibit-summary-card">
-          <img src="https://via.placeholder.com/340x200" />
-        </div>
-        <div className="internal-jump-links">
-          <Link to="/exhibits/">More Exhibits</Link>
-        </div>                           
-      </section>
+      <h2>Unlocking the Airwaves</h2>
+      <p>
+        Unlocking the Airwaves: Revitalizing an Early Public and Educational
+        Radio Collection is a comprehensive online collection of early
+        educational public radio content from the National Association of
+        Educational Broadcasters (NAEB). The forerunner of CPB and its arms, NPR
+        and PBS, the NAEB developed and distributed educational radio programs
+        and accompanying print materials to schools and communities across the
+        United States. What’s more, the NAEB lobbied extensively to unlock the
+        airwaves—to access precious frequency space—in order to bring the voices
+        of poet Robert Frost, architect Frank Lloyd Wright, anthropologist
+        Margaret Mead, and conservationist “Ranger Mac,” among many other
+        individuals, into American homes and classrooms.
+      </p>
+      <p>
+        The NAEB’s history is the dramatic story of idealists who believed in
+        the utopian possibilities of technology for education and social uplift
+        and who faced considerable challenges in pursuit of those goals,
+        including economic depression, world war, and the scarcity of the
+        electromagnetic spectrum. It’s a story that has much to tell us about
+        20th century American culture, as well as the 21st century’s environment
+        of online educational technology and podcasting that we live in today.
+      </p>
+      <p>
+        Despite its historic importance and contemporary relevance, most of the
+        NAEB members’ programs were never heard again after their initial brief
+        moments on the air. The archives for the radio programs and their
+        related paper documentation have been split for over 25 years between
+        two institutions: the University of Maryland and the Wisconsin
+        Historical Society. Unlocking the Airwaves reunites the split
+        collections, ﬁnally realizing the potential of the collections of the
+        NAEB for exploration and and the broader public.
+      </p>
+      <div className="internal-jump-links">
+        <Link to="/about/">Read More about the Project</Link>
+        <Link to="/explore/">Explore the Archive</Link>
+      </div>
+    </section>
+    <section>
+      <h2>Featured Exhibits</h2>
+      <FeaturedExhibitsProvider />
+    </section>
   </Layout>
 )
 
