b5234a3e8a4b46fa0b07c7238b89a7668b84219b
trevormunoz <trevor@trevormunoz.com>
2021-05-11 11:22:08 -0400

    Make project team also CMS-editable

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import remark from "remark"
+import remarkHtml from "remark-html"
+import recommended from "remark-preset-lint-recommended"
 
 import "./about.css"
+
+const convertMarkdown = remark()
+  .use(recommended)
+  .use(remarkHtml).processSync
 
 const AboutPage = ({ data }) => {
   const cmsData = data.allMarkdownRemark.nodes[0]
@@ -11,6 +18,11 @@ const AboutPage = ({ data }) => {
     <Layout url="/about/">
       <div className="page-about">
         <section dangerouslySetInnerHTML={{ __html: cmsData.html }}></section>
+        <section
+          dangerouslySetInnerHTML={{
+            __html: convertMarkdown(cmsData.frontmatter.team),
+          }}
+        ></section>
       </div>
     </Layout>
   )
@@ -22,6 +34,9 @@ export const query = graphql`
       nodes {
         html
         fileAbsolutePath
+        frontmatter {
+          team
+        }
       }
     }
   }
