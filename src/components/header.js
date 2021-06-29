3756e38b4e45078aa3db83e9ffb79e1c6f411202
trevormunoz <trevor@trevormunoz.com>
2021-06-29 17:06:13 -0400

    Accessibility fixes to header and footer

--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,23 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import './header.css'
-import Logo from '../svg/uta-header.svg'
-import MobileLogo from '../svg/uta-header-core_only.svg'
-import Nav from './nav'
+import "./header.css"
+import Logo from "../svg/uta-header.svg"
+import MobileLogo from "../svg/uta-header-core_only.svg"
+import Nav from "./nav"
 
 const Header = () => {
-
-    return (
-      <header>
-        <div className="header-content">
-          <Link to="/">
-            <Logo className="main-logo" alt="Unlocking the Airwaves" />
-            <MobileLogo className="alt-logo" alt="Unlocking the Airwaves" />
-          </Link>
-          <Nav />
-        </div>
-      </header>
-    )
+  return (
+    <header>
+      <div className="header-content">
+        <Link aria-label="Unlocking the Airwaves home page" to="/">
+          <Logo className="main-logo" alt="" />
+          <MobileLogo className="alt-logo" alt="" />
+        </Link>
+        <Nav />
+      </div>
+    </header>
+  )
 }
 
 Header.propTypes = {
