a20fb1dc4a8e85ad09323d693e4ed787f0dd8127
ef345 <83036739+ef345@users.noreply.github.com>
2021-07-02 10:54:16 -0400

    Add title and blurb for map visualization

--- src/components/map-viz.js
+++ src/components/map-viz.js
@@ -3,15 +3,18 @@ import React from "react"
 export default function MapViz() {
   return (
     <section id="map">
-      <h2>Title Me</h2>
+      <h2>Map of U.S. Organizations in the NAEB</h2>
       <p>
-        Bacon ipsum dolor amet prosciutto ut leberkas, beef tail biltong
-        turducken. Salami culpa doner esse, cupim aliqua rump ut. Ad consequat
-        burgdoggen ex in venison. Beef ribs sausage ex, pork chop pork aliqua
-        short loin in tri-tip culpa andouille cupidatat aliquip biltong. Veniam
-        occaecat aliqua ribeye, boudin do pork chop pork belly eiusmod aute
-        alcatra ex picanha. Eiusmod dolore fugiat meatloaf frankfurter. Sint in
-        in leberkas picanha porchetta reprehenderit.
+        This map lets you explore some of the major organizations who participated in 
+        the National Association of Educational Broadcasters, such as radio stations, 
+        universities, broadcasting companies, and other educational organizations. Each 
+        black dot on the map represents one organization, and you can click to get more 
+        information about an organization and a link to that organization's landing page on 
+        Unlocking the Airwaves, which includes all the collection materials the organization 
+        created or contributed to. The states are shaded to represent their NAEB region, an 
+        organizational structure within the NAEB that helped geographically close NAEB members 
+        to meet and collaborate. Pan and zoom around the map using your mouse or the tools in 
+        the upper left corner.
       </p>
       <iframe
         title="Unlocking the Airwaves - U.S. Map of Major Organizations"
