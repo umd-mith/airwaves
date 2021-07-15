umd-mith/airwaves

commit 411b890728c2ec00e0c828ebcde4fac4a45c1fa8

showThemes
This commit adds a showThemes method on the Search page, which is pushed
down into Search component to get called with true or false depending on
whether there are any results or not. This allows the themes to show up
only when there are no results.

In addition this commit also updates the browser location when facets
are selected, so searches are shareable as URLs.

src/pages/search.js
@@ -15,6 +15,7 @@ class SearchPage extends React.Component {
     this.state = {
       query: qs.q,
       facets: qs.f,
+      showThemes: true
     }
   }
 
@@ -28,17 +29,20 @@ class SearchPage extends React.Component {
     })
   }
 
+  showThemes(show) {
+    this.setState({
+      showThemes: show
+    })
+  }
+
   render() {
     const themeGroups = this.props.data.allThemesJson.group
-
-    // only display themes when a query or facet selection has *not* been made
-    const showThemes = ! (this.state.query || this.state.facets)  
 
     return (
       <Layout title="Search">
         <Loader>
-          <Search query={this.state.query} facets={this.state.facets} />
-          <div className="themes" style={{display: showThemes ? 'block' : 'none'}}>
+          <Search query={this.state.query} facets={this.state.facets} showThemes={e => this.showThemes(e)} />
+          <div className="themes" style={{display: this.state.showThemes ? 'block' : 'none'}}>
             {themeGroups.map(tg => {
               const groupKey = tg.fieldValue.replace(/[&,.\s]/g, "")
               return (

src/components/search.js
@@ -90,8 +90,6 @@ class Search extends Component {
     const results = this.state.results
     const displayedResults = this.state.displayedResults
 
-    console.log(`percent viewed ${percentViewed}`)
-
     if (
       percentViewed > 0.8 &&
       millisSinceLastUpdate > 1000 &&
@@ -177,6 +175,12 @@ class Search extends Component {
       results: results,
       displayedResults: displayedResults,
     })
+
+    if (results.length === 0) {
+      this.props.showThemes(true)
+    } else {
+      this.props.showThemes(false)
+    }
 
     this.setLocation(query, facets)
 

commit 3bf9461ef7b77061677ab0399fbf3e4599ba5e30

Fix theme display regression
Introduced in 411b89072

src/pages/search.js
@@ -49,7 +49,7 @@ class SearchPage extends React.Component {
             />
             <div
               className="themes"
-              style={{ display: this.state.showThemes ? "block" : "none" }}
+              style={{ display: this.state.showThemes ? "grid" : "none" }}
             >
               {themeGroups.map(tg => {
                 const groupKey = tg.fieldValue.replace(/[&,.\s]/g, "")