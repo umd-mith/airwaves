8796ec23f370af83dd5f3c2cb0f620d5e8cebbba
ssapienza <sapienza@umd.edu>
2021-07-22 15:26:54 -0400

    Added intro blurb for Organizations landing page

--- src/pages/organizations.js
+++ src/pages/organizations.js
@@ -15,6 +15,9 @@ const Organizations = ({ data }) => {
             </Link>{" "}
             Browse Organizations
           </h1>
+            <p> 
+              Listed below are organizations represented in the NAEB's programs and documents. Click on an organization's name to go to their landing page with brief biographical information from Wikidata and Wikipedia, as well as links to associated programs and documents on <em>Unlocking the Airwaves</em>. Each organization has a unique ID that comes from the Wikidata knowledge base, which begins with the letter 'Q' in the URL of each landing page. This identifier can also be used to locate the organization's record on <a href="https://www.wikidata.org">Wikidata</a>, which contains links to other references to the person across the web (Library of Congress, VIAF, and more).
+            </p>
         </section>
         <section>
           <ul className="long-list">
