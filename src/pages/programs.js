b3f7cd938223c7370f47356f7ec7962ff6015100
Ed Summers <ehs@pobox.com>
2021-07-22 16:45:55 -0400

    Icon no longer needed since it's in Registry component.

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-// import { FaAngleUp } from "react-icons/fa"
 
 import Layout from "../components/layout"
 import Registry from "../components/registry"
